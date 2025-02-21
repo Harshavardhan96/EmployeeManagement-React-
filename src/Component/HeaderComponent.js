a3917fdabbebf1cd6c373660ad62ad73bfeca253
Harshavardhan96 <harshavardhansai369@gmail.com>
2025-02-21 15:54:06 +0530

    modified

--- src/Component/HeaderComponent.js
+++ src/Component/HeaderComponent.js
@@ -1,11 +1,13 @@
 import React from 'react'
+// import { Link } from 'react-router-dom';
 
 const HeaderComponent = () => {
   return (
     <div>
         <header>
             <nav className='navbar navbar-dark bg-dark'>
-                <a className="navbar-brand text-center" href="#" >Employee Management System</a>
+                <a className="navbar-brand text-center" href="/" >Employee Management System</a>
+                {/* <Link className="navbar-brand text-center" to="/">Employee Management System</Link> */}
             </nav>
         </header>
     </div>
